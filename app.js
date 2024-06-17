d2c8acaaef29ec3d37311c08e7167932b78e6196
Andrew Weller <andrew@weller.fun>
2024-06-16 20:31:55 -0700

    first prototype

--- app.js
+++ app.js
@@ -1,17 +1,55 @@
-// const synth = new Tone.Synth().toDestination();
+const synth = new Tone.Synth().toDestination();
 
-// synth.triggerAttackRelease("C4", "8n");
+const rows = 5;
+const cols = 5;
+const buttonInfo = [];
+const middleC = 261.626;
+const harmonicSeriesStart = 4;
+
+function getPitch(r, c) {
+    var pitch = middleC;
+    const h = harmonicSeriesStart;
+
+    for (var i = 0; i < r; i++) {
+        pitch *= (i + (h + 1)) / (i + h);
+    }
+    for (var i = 0; i < c; i++) {
+        pitch *= (i + h) / (i + (h + 1));
+    }
+    return pitch;
+}
+
+function buttonClick(r, c) {
+    var pitch = getPitch(r, c);
+    console.log(r, c, pitch);
+    synth.triggerAttackRelease(pitch, "8n");
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.getElementById('grid');
     const buttons = [];
-    const rows = 5;
-    const cols = 5;
 
     for (let i = 0; i < rows * cols; i++) {
         const button = document.createElement('button');
-        button.innerHTML = `<span>${i + 1}</span>`;  // Optional: Add numbers to buttons for reference
+        button.innerHTML = `<span></span>`;  // Optional: Add numbers to buttons for reference
         grid.appendChild(button);
         buttons.push(button);
+
+        var r = Math.floor(i / rows);
+        var c = i % cols;
+        var pitch = getPitch(r, c);
+
+        var red = 76;
+        var green = 175;
+        var blue = 80;
+
+        var colorScale = 0.25 + (pitch / getPitch(rows, cols)) * 0.5;
+        
+        button.style.backgroundColor = `rgb(${red * colorScale}, ${green * colorScale}, ${blue * colorScale})`;
+        button.onclick = function() {
+            //buttonClick(Math.floor(i / rows), i % cols);
+            buttonClick(Math.floor(i / rows), i % cols);
+        }
     }
 
     // Store buttons in a 2D array
